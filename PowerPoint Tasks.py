b33a19623dcc6083ae366e41213741034953e1fd
c3670915 <mckeownbobby4@gmail.com>
2023-11-09 11:54:59 +0000

    yes

---
+++ PowerPoint Tasks.py
@@ -0,0 +1,18 @@
+import random
+
+# High-Low
+
+Number = random.randrange(0, 100)
+print(Number)
+UserGuess = 0
+while UserGuess != Number:
+    UserGuess = input("PLease enter a number and the computer will state higher or lower ")
+    if UserGuess > str(Number):
+        print("Lower")
+    elif UserGuess < str(Number):
+        print("Higher")
+    else:
+        print("Correct")
+        break
+
+
---
+++ Practical Tasks.py
@@ -0,0 +1,31 @@
+# TASK1
+names = {"John", "Eric", "Terry", "Michael", "Graham", "Terry"}
+print(names)
+# TASK2
+sentence = "this is a sentence containing some letters"
+unique_letters = {x for x in sentence}
+print(unique_letters)
+# TASK3
+name = input("Enter your name: ")
+if name not in names:
+    print("You are NOT listed in the set of known names")
+
+# help(set)
+# TASK4
+staff = {"Pete", "Kelly", "Jon", "Paul", "Sally", "Sue"}
+directors = {"Kelly", "Rupert", "Cyril", "Jon"}
+
+staff = staff.union({"Paul", "Mckeown"})
+print(staff)
+
+external = staff.difference({"Kelly"})
+print(external)
+
+staff = staff.intersection({"Kelly", "Bob"})
+print(staff)
+
+staff = staff.symmetric_difference(directors)
+print(staff)
+#TASK5
+vowels = {"a", "e", "i"}
+vowels = vowels.union("o", "u")
